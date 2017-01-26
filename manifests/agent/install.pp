f941bea75085a3bb61eca5c61095b10074d6cecf
Dan <dan@ifunky.net>
2017-01-26 14:49:15 +0000

    Remove unused java installers

--- manifests/agent/install.pp
+++ manifests/agent/install.pp
@@ -5,23 +5,9 @@ class teamcity::agent::install {
 
   include ::teamcity::agent
 
-  #windows::java { 'install java jdk':
-  #  update      => '51',
-  #  arch        => $::architecture,
-  #  type        => 'jdk',
-  #}
-
-  #windows::java { 'install java jre':
-  #  update      => '51',
-  #  arch        => $::architecture,
-  #  type        => 'jre',
-  #  require    => Windows::Java['install java jdk']
-  #}
-
   package { 'teamcityagent':
     ensure          => '2.0.0',
     install_options => ['-params','"',"serverurl=${teamcity::agent::server_url}","ownPort=${teamcity::agent::agent_port}","agentName=${teamcity::agent::agent_name}","agentDir=d://TeamCity//BuildAgent",'"',"--allow-empty-checksums"],
     provider        => 'chocolatey',
-    #require         => Windows::Java['install java jre']
   }
 }
