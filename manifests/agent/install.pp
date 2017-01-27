0c92a53756a719b714698d118b3311d50fa2d6fb
Dan <dan@ifunky.net>
2017-01-27 10:35:29 +0000

    Adding VS v14.0 web targets

--- manifests/agent/install.pp
+++ manifests/agent/install.pp
@@ -5,10 +5,16 @@ class teamcity::agent::install {
 
   include ::teamcity::agent
 
+  $web_targets = [ 'C:\\Program Files (x86)\\MSBuild\\Microsoft' ]
+
   package { 'teamcityagent':
     ensure          => '2.0.0',
     install_options => ['-params','"',"serverurl=${teamcity::agent::server_url}","ownPort=${teamcity::agent::agent_port}","agentName=${teamcity::agent::agent_name}","agentDir=d://TeamCity//BuildAgent",'"',"--allow-empty-checksums"],
     provider        => 'chocolatey',
+  }
+
+  file { $web_targets:
+    ensure             => directory,
   }
 
   file { 'Visual Studio Web Targets':
@@ -17,5 +23,6 @@ class teamcity::agent::install {
     source             => 'puppet:///modules/teamcity/VisualStudio/',
     source_permissions => ignore,
     recurse            => true,
+    require			 => File[$web_targets]
   }
 }
